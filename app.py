090bf72e2b5f4cf7d534eee8af162410ea63f174
alvinadjei <alvinadjei@college.harvard.edu>
2025-05-02 16:32:07 -0700

    Switch to Flask app
    No reason to use Nextjs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 import numpy as np
 import cv2
 
@@ -40,6 +40,9 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/video')
 def video():
